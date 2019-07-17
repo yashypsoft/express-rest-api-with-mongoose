e3cb776e0b38ade437b34dc942620735c40072ab
Yash Prajapati <yashypsoft@gmail.com>
2019-07-17 22:07:37 +0530

    cookie

--- app.js
+++ app.js
@@ -38,36 +38,49 @@ app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
+app.use(cookieParser("12345-67890-09876-54321"));
 
 //basic auth
 function auth(req, res, next) {
-  console.log(req.headers);
+  console.log(req.signedCookies);
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+      var err = new Error("you are noot authenticated");
 
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-    var err = new Error("you are noot authenticated");
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
+    var auth = new Buffer.from(authHeader.split(" ")[1], "base64")
+      .toString()
+      .split(":");
 
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    return next(err);
+    var username = auth[0];
+    var password = auth[1];
+
+    if (username == "admin" && password == "password") {
+      res.cookie('user','admin',{signed : true});
+      next();
+    } else {
+      var err = new Error("you are noot authenticated");
+
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
   }
-  var auth = new Buffer(authHeader.split(" ")[1], "base64")
-    .toString()
-    .split(":");
+  else{
+    if(req.signedCookies.user === 'admin'){
+      next();
+    }
+    else{
+      var err = new Error("you are noot authenticated");
 
-  var username = auth[0];
-  var password = auth[1];
-
-  if (username == "admin" && password == "password") {
-    next();
-  } else {
-    var err = new Error("you are noot authenticated");
-
-    res.setHeader("WWW-Authenticate", "Basic");
-    err.status = 401;
-    return next(err);
+      res.setHeader("WWW-Authenticate", "Basic");
+      err.status = 401;
+      return next(err);
+    }
   }
 }
 
